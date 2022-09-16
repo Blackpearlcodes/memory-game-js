9e8730cb8ffd7c3e459661c5748f5aecf37c6a10
Ghassan Fares <gfares@gmail.com>
2022-09-16 23:45:23 +0300

    html css js edit

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
 var mem = {
 
     hWrap : null,
-
-    url : "https://code-boxx.com/wp-content/uploads/2019/08/", // optional, url to images
-    sets : 3, // number of sets to match
-    grid : [], // current game grid
-    moves : 0, // total number of moves
-    matched : 0, // number of sets that have been matched
-    last : null, // last opened card
-    lock : null, // timer, lock game controls when showing mismatched cards
-    hint : 1000, // how long to show mismatched cards
+    url : "assets/",
+    sets : 3,
+    grid : [],
+    moves : 0,
+    matched : 0,
+    last : null,
+    lock : null,
+    hint : 1000,
   
     // (B) PRELOAD
     preload : () => {
-      // (B1) GET HTML GAME WRAPPER
-      mem.hWrap = document.getElementById("mem-game");
+
+        mem.hWrap = document.getElementById("main-game");
   
-      // (B2) PRELOAD IMAGES
-      let img, loaded = -1;
+
+        let img, loaded = -1;
       for (let i=0; i<=mem.sets; i++) {
         img = document.createElement("img");
         img.onload = () => {
           loaded++;
           if (loaded == mem.sets) { mem.reset(); }
         };
-        img.src = `${mem.url}smiley-${i}.png`;
+        img.src = "assets/" + `img-${i}.png`; 
       }
     },
   
-    // (C) RESET GAME
+    // RESET GAME
     reset : () => {
-      // (C1) RESET ALL FLAGS
+      // RESET FLAGS
       clearTimeout(mem.lock); mem.lock = null;
       mem.moves = 0; mem.matched = 0;
       mem.last = null; mem.grid = [];
@@ -38,8 +37,7 @@ var mem = {
         mem.grid.push(s); mem.grid.push(s);
       }
   
-      // (C2) RANDOM RESHUFFLE CARDS
-      // credits : https://gomakethings.com/how-to-shuffle-an-array-with-vanilla-js/
+      // RANDOM CARDS
       let current = mem.sets * 2, temp, random;
       while (0 !== current) {
         random = Math.floor(Math.random() * current);
@@ -48,14 +46,13 @@ var mem = {
         mem.grid[current] = mem.grid[random];
         mem.grid[random] = temp;
       }
-      // console.log(mem.grid); // CHEAT
   
-      // (C3) CREATE HTML CARDS
+      // CREATE CARDS
       mem.hWrap.innerHTML = "";
       for (let id in mem.grid) {
         let card = document.createElement("img");
         card.className = "mem-card";
-        card.src = `${mem.url}smiley-0.png`;
+        card.src = `${mem.url}img-0.png`;
         card.onclick = () => { mem.open(card); };
         card.set = mem.grid[id];
         card.open = false;
@@ -64,24 +61,24 @@ var mem = {
       }
     },
   
-    // (D) OPEN A CARD
+    // OPEN CARD
     open : (card) => { if (mem.lock == null) { if (!card.open) {
-      // (D1) UPDATE FLAGS & HTML
+      // UPDATE FLAGS
       card.open = true;
       mem.moves++;
-      card.src = `${mem.url}smiley-${card.set}.png`;
+      card.src = `${mem.url}img-${card.set}.png`;
       card.classList.add("open");
   
-      // (D2) FIRST CARD - SET IN LAST
+      // FIRST CARD - SET IN LAST
       if (mem.last == null) { mem.last = card; }
   
       // (D3) SECOND CARD - CHECK MATCH
       else {
-        // (D3-1) REMOVE CSS CLASS
+        // REMOVE CSS CLASS
         card.classList.remove("open");
         mem.last.classList.remove("open");
   
-        // (D3-2) MATCHED
+        // MATCHED
         if (card.set == mem.last.set) {
           // UPDATE FLAGS + CSS
           mem.matched++;
@@ -96,7 +93,6 @@ var mem = {
           }
         }
   
-        // (D3-3) NOT MATCHED - CLOSE BOTH CARDS ONLY AFTER A WHILE
         else {
           card.classList.add("wrong");
           mem.last.classList.add("wrong");
@@ -105,8 +101,8 @@ var mem = {
             mem.last.classList.remove("wrong");
             card.open = false;
             mem.last.open = false;
-            card.src = `${mem.url}smiley-0.png`;
-            mem.last.src = `${mem.url}smiley-0.png`;
+            card.src = `${mem.url}img-0.png`;
+            mem.last.src = `${mem.url}img-0.png`;
             mem.last = null;
             mem.lock = null;
           }, mem.hint);
